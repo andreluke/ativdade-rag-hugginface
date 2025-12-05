49092c8e0a48142bde78de9b282cef32392405c8
andreluke <andre.l.sales2706@gmail.com>
2025-12-05 11:22:39 -0300

    :broom: clean: Remoção de arquivos

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-python -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-
-echo "Ambiente pronto. Ative com: source .venv/bin/activate"
